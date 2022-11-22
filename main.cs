7f171c28d94ed229d2c79aea5372e9821176f415
Roxnnis <60492921+Nosirn@users.noreply.github.com>
2022-11-22 01:33:13 +0100

    Fichier test simple

--- main.cs
+++ main.cs
@@ -1,7 +1,10 @@
-namespace Main
+using static Sort;
+using static Mot;
+public class MainClass
 {
-	public static void main()
+	public static void Main()
 	{
-		Console.WriteLine("Salut :D");
+		Sort s = new Sort("Test", new Ecriture(Element.EAU,Cible.SOI,Numerique.CONSTANTE), "OuiOuiOui !");
+		Console.WriteLine(s);
 	}
 }
